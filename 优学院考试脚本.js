704c3d862ea8d8b01ba3a12c3fda87cbf2106c81
sun wei <1793786487@qq.com>
2020-04-14 12:08:51 +0800

    "初次提交"

---
+++ 优学院考试脚本.js
@@ -0,0 +1,22 @@
+var question= document.getElementsByClassName("multiple-choice record")
+var arr =Array.from(question)
+
+ function get(id,back){
+    $.ajax({
+        async: false,
+        type: "get",
+        url: "https://api.ulearning.cn/questionAnswer/" + id,
+        datatype: "json",
+        success: function(result) {
+            console.log(result.correctAnswerList);
+            back(result.correctAnswerList);
+        }
+      });
+ }
+
+ arr.forEach(one=>{
+    let id= one.dataset.id;
+    get(id,function(Answer){
+        Array.from(one.children)[0].innerText+="=========´ð°¸ÊÇ:"+Answer;
+    })
+ })
